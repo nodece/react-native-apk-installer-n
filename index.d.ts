50bb8817d6cc1600ba09235b96ab0ca94488b090
Zixuan Liu <nodeces@gmail.com>
2020-04-06 21:34:06 +0800

    Add declare file

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare class RNApkInstallerN {
+    static install(): Promise<string>;
+}
+export default RNApkInstallerN;
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import { NativeModules } from 'react-native';
 
 const { RNApkInstallerN } = NativeModules;
